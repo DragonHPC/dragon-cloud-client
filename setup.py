723b08bc64793aeddd3d25cc1ffdc13ce51a00d5
Maria Kalantzi <maria.kalantzi@hpe.com>
2025-06-24 14:35:42 -0700

    Add python 3.10+ requirement

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="dragon-api-client",
     version="0.1.0",
     description="Python API Client and CLI for the Dragon REST Python API",
+    python_requires=">=3.10",
     packages=find_packages(),
     install_requires=["requests", "PyYAML", "click"],
     entry_points={
